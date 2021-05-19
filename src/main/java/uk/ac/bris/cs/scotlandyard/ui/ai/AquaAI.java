d2b5279c849f57142785993a1376769fd61caa3e
Hellin <sun>
2021-05-19 21:14:06 +0100

    made mrX save doubleMove when not necessary

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/AquaAI.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/AquaAI.java
@@ -1,7 +1,9 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import javax.annotation.Nonnull;
@@ -26,17 +28,20 @@ public class AquaAI implements Ai {
 
 	public Move pickBestMove(Board board, ImmutableList<Move> moves) {
 		Move bestMove = null;
-		int bestScore = -1;
+		int bestScore = -100;
 		for (Move moveMade : moves) { // get the move with the best score (higher = better)
 			int score = scoreBoard(board, moveMade);
 			if (score > bestScore) { // check if better than best
+				bestScore = score;
 				bestMove = moveMade;
 			}
 		}
+
 		return bestMove;
 	}
 
 	public int scoreBoard(Board board, Move moveMade) {
+		int mrXSourceLocation = moveMade.source();
 		int newMrXLocation;
 		if (moveMade instanceof Move.SingleMove) { // get destination if single move made
 			newMrXLocation = ((Move.SingleMove) moveMade).destination;
@@ -45,19 +50,41 @@ public class AquaAI implements Ai {
 			newMrXLocation = ((Move.DoubleMove) moveMade).destination2;
 		}
 
+		boolean isDetectiveNearby = false; // Save doublemove when there's no detective nearby at the original place
+
+		Set <Integer> adjacentNodes = new HashSet<>();
+		adjacentNodes.addAll(board.getSetup().graph.adjacentNodes(newMrXLocation)); // adjacentNodes after move
+		int score = 0;
+
 		var players = board.getPlayers();
 		HashMap<Piece, Optional<Integer>> detectiveLocations = new HashMap<>();
 		Optional<Integer> detectiveLocation;
 		int averageDistanceFromDetectives = 0;
+
+
+
 		for (Piece player : players) {
 			if (!player.isDetective()) continue; // exclude mrX
 			detectiveLocation = board.getDetectiveLocation((Piece.Detective) player); // get location of detective
 			if (detectiveLocation.isEmpty()) { // check to be sure we have a location, this shouldn't happen
 				throw new IllegalArgumentException("No detective location found.");
 			}
+			if(board.getSetup().graph.adjacentNodes(detectiveLocation.get()).contains(mrXSourceLocation)){
+				isDetectiveNearby = true; // check if there's detective nearby
+			}
+			if(board.getSetup().graph.adjacentNodes(detectiveLocation.get()).contains(newMrXLocation)){
+				score = score - 1; // check how many detectives are close to mrX after the move
+			}
 			detectiveLocations.put(player, detectiveLocation); // add location of this detective
 			averageDistanceFromDetectives += Math.abs(newMrXLocation - detectiveLocation.get()); // get absolute value of naive distance from mrX (after move made) to this detective
 		}
-		return averageDistanceFromDetectives / (board.getPlayers().size() - 1); // divide by number of detectives
+
+
+		System.out.println(isDetectiveNearby);
+		System.out.println(averageDistanceFromDetectives / (board.getPlayers().size() - 1));
+
+		if ((moveMade instanceof Move.DoubleMove) && !isDetectiveNearby) return 0; // Save doublemove when there's no detective nearby
+		else if(score < 0) return score; // this move would be negative if there's detective nearby after move
+		else return averageDistanceFromDetectives / (board.getPlayers().size() - 1); // divide by number of detectives
 	}
 }
