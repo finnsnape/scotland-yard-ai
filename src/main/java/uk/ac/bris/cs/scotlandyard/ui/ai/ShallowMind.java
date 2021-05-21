2241813792a06b10466a07e27fa7b4c8a83ff2e7
Finn Snape <snapefinn@gmail.com>
2021-05-21 04:58:04 +0100

    cleaned up a lot, fixed many

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/ShallowMind.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/ShallowMind.java
@@ -1,7 +1,5 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai;
 
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
@@ -27,101 +25,97 @@ public class ShallowMind implements Ai {
 	}
 
 	public Move pickBestMove(MutableValueGraph stateGraph, Board board) {
-		/*
-		idea is to go through the graph and find the path that results in the highest overall total score (sum of the edges)
-		we can then retrieve the move we need to make with node.moveMade
-
-		TODO: currently only takes the first node, but it needs to work recursively and then return the move that leads us to the best outcome
-		 */
-		GraphNode topNode = new GraphNode((Board.GameState) board, null);
-		Set<GraphNode> nextNodes = stateGraph.successors(topNode);
+		GraphNode topNode = new GraphNode((Board.GameState) board, null); // get root of the tree
+		Set<GraphNode> nextNodes = stateGraph.successors(topNode); // get all nodes in layer below root (i.e. all MrX moves)
 		Move bestMove = null;
-		int bestScore = -100;
+		int bestScore = 0;
 		for (GraphNode node : nextNodes) { // find the node which has the highest score
-			Integer nodeScore = (Integer) stateGraph.edgeValue(topNode, node).get();
-			if (nodeScore > bestScore) {
+			int nodeScore = (Integer) stateGraph.edgeValue(topNode, node).get(); // get the score associated with this MrX move
+			int detectiveScore = 0;
+			Set<GraphNode> nextNodesDetective = stateGraph.successors(node); // get all the nodes in layer below this MrX move (i.e. all possible detective moves afterwards)
+			if (!nextNodesDetective.isEmpty()) { // make sure not empty (i.e. move before the last move), else dividing by 0
+				for (GraphNode nodeDetective : nextNodesDetective) {
+					detectiveScore += (Integer) stateGraph.edgeValue(node, nodeDetective).get(); // get the score associated with this detective move
+				}
+				detectiveScore = detectiveScore / nextNodesDetective.size(); // get the average of the scores for the possible detective moves from this MrX move
+			}
+			if (nodeScore + detectiveScore > bestScore) { // update best move if necessary
 				bestMove = node.moveMade();
-				bestScore = nodeScore;
+				bestScore = nodeScore + detectiveScore;
 			}
 		}
-
 		return bestMove;
 	}
 
 	public int scoreState(Board.GameState gameState, Move moveMade) {
-		/*
-		TODO: use a less naive approach
-		 */
-		int mrXSourceLocation = moveMade.source(); //mrx starting location
-		boolean isDetectiveNearby = false; // Save doublemove when there's no detective nearby at the original place
+		int NEARBY_DETECTIVE_PENALTY = 10;
+		int FREEDOM_NODES_MULTIPLIER = 4;
+		int UNNECESSARY_DOUBLEMOVE_PENALTY = 20;
+		int REPEATED_MOVE_PENALTY = 1;
+
+		int mrXSourceLocation = moveMade.source(); // MrX location before move made
+		boolean isDetectiveNearby = false;
 
 		int newMrXLocation;
 		int score = 0;
 
-		if (moveMade instanceof Move.SingleMove) { // get destination if single move made
+		if (moveMade instanceof Move.SingleMove) { // get MrX destination if single move made
 			newMrXLocation = ((Move.SingleMove) moveMade).destination;
 		}
-		else { // get destination if double move made
+		else { // get MrX destination if double move made
 			newMrXLocation = ((Move.DoubleMove) moveMade).destination2;
 		}
 
-		Set <Integer> adjacentNodes = new HashSet<>();
-		adjacentNodes.addAll(gameState.getSetup().graph.adjacentNodes(newMrXLocation)); // adjacentNodes after move
-
+		Set <Integer> adjacentNodesBefore = gameState.getSetup().graph.adjacentNodes(mrXSourceLocation); // MrX's adjacentNodes before move
+		Set <Integer> adjacentNodesAfter = gameState.getSetup().graph.adjacentNodes(newMrXLocation); // MrX's adjacentNodes after move
 
 		ImmutableSet<Piece> players = gameState.getPlayers();
-		HashMap<Piece, Optional<Integer>> detectiveLocations = new HashMap<>();
-		Optional<Integer> detectiveLocation;
-		int DistanceFromDetectives = 0;
+		int totalDistanceFromDetectives = 0;
 
 		for (Piece player : players) {
-			if (!player.isDetective()) continue; // exclude mrX
-			detectiveLocation = gameState.getDetectiveLocation((Piece.Detective) player); // get location of detective
+			if (player.isMrX()) continue; // exclude mrX
+			Optional<Integer> detectiveLocation = gameState.getDetectiveLocation((Piece.Detective) player); // get location of detective
 			if (detectiveLocation.isEmpty()) { // check to be sure we have a location, this shouldn't happen
 				throw new IllegalArgumentException("No detective location found.");
 			}
-			if(gameState.getSetup().graph.adjacentNodes(detectiveLocation.get()).contains(mrXSourceLocation)){
-				isDetectiveNearby = true; // check if there's detective nearby before moving
+			if (adjacentNodesBefore.contains(detectiveLocation.get())) {
+				isDetectiveNearby = true; // this detective is in the nodes adjacent to MrX before the move is made
 			}
-			if(gameState.getSetup().graph.adjacentNodes(detectiveLocation.get()).contains(newMrXLocation)){
-				score = score - 1; // check how many detectives are close to mrX after the move
+			if (adjacentNodesAfter.contains(detectiveLocation.get())) {
+				score -= NEARBY_DETECTIVE_PENALTY; // punish move because it will result in MrX being directly next to a detective
 			}
-
-			detectiveLocations.put(player, detectiveLocation); // add location of this detective
-			DistanceFromDetectives += Math.abs(newMrXLocation - detectiveLocation.get()); // get absolute value of naive distance from mrX (after move made) to this detective
+			totalDistanceFromDetectives += Math.abs(newMrXLocation - detectiveLocation.get()); // get absolute value of naive distance from mrX (after move made) to this detective
 		}
-		int averageDistanceFromDetectives = DistanceFromDetectives / (gameState.getPlayers().size() - 1);// divide by number of detectives
-		//choose a move that has more freedom
-		averageDistanceFromDetectives += (gameState.getSetup().graph.adjacentNodes(newMrXLocation).size() * 10);
 
-
-
-		if(score < 0) return score; // this move would be negative if there's detective nearby after move
-		else if ((moveMade instanceof Move.DoubleMove) && !isDetectiveNearby) return 0; // Save doublemove when there's no detective nearby
-		else return averageDistanceFromDetectives;
-
+		int averageDistanceFromDetectives = totalDistanceFromDetectives / (players.size() - 1); // divide by number of detectives to get average
+		averageDistanceFromDetectives += adjacentNodesAfter.size() * FREEDOM_NODES_MULTIPLIER; // give higher score for moves that result in more freedom
+		score = averageDistanceFromDetectives;
+		if (moveMade instanceof Move.DoubleMove && !isDetectiveNearby) {
+			score -= UNNECESSARY_DOUBLEMOVE_PENALTY; // we want to save double move when there is no detective nearby
+		}
+		if (newMrXLocation == mrXSourceLocation) {
+			score -= REPEATED_MOVE_PENALTY; // punish move where we end up in the same place
+		}
+		return score;
 	}
 
 	public MutableValueGraph generateGraph(Board board) {
-		/*
-		this should work (recursively?) for a given number of layers
-		e.g. evaluate MrX's possible moves and then all possible detective moves as a result, then all possible detective moves again
-		 */
 		Board.GameState gameState = (Board.GameState) board;
-		Board.GameState gameStateTemp;
 		MutableValueGraph<GraphNode, Integer> stateGraph = ValueGraphBuilder.directed().build();
-		ImmutableList<Move> moves = gameState.getAvailableMoves().asList();
-
-		GraphNode topNode = new GraphNode(gameState, null);
-		GraphNode tempNode;
-		int score;
-		for (Move move : moves) {
-			score = scoreState(gameState, move); // score the move we want to make
-			gameStateTemp = gameState.advance(move); // establish the new gamestate after move has been made
-			tempNode = new GraphNode(gameStateTemp, move); // add new gamestate and movemade to node
-			stateGraph.putEdgeValue(topNode, tempNode, score); // add node and score to the graph
+		ImmutableList<Move> movesMrX = gameState.getAvailableMoves().asList(); // all possible MrX moves
+		GraphNode topNode = new GraphNode(gameState, null);; // initial gamestate
+		for (Move MrXMove : movesMrX) {
+			int score = scoreState(gameState, MrXMove); // score the move we want to make
+			Board.GameState gameStateTempMrX = gameState.advance(MrXMove); // establish the new gamestate after move has been made
+			GraphNode tempNodeMrX = new GraphNode(gameStateTempMrX, MrXMove); // add new gamestate and movemade to node
+			stateGraph.putEdgeValue(topNode, tempNodeMrX, score); // add node and score to the graph
+			ImmutableSet<Move> resultantMovesDetective = gameStateTempMrX.getAvailableMoves(); // all possible moves for detectives after this MrX move
+			for (Move detectiveMove : resultantMovesDetective) {
+				Board.GameState gameStateTempDetective = gameStateTempMrX.advance(detectiveMove); // make the detective move
+				GraphNode tempNodeDetective = new GraphNode(gameStateTempDetective, detectiveMove); // make node with this gamestate and the required detective move
+				stateGraph.putEdgeValue(tempNodeMrX, tempNodeDetective, scoreState(gameStateTempDetective, detectiveMove)); // add node with its score in layer beneath original MrX move
+			}
 		}
 		return stateGraph;
-
 	}
 }
